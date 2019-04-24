894791cb15cc6809639d01281730c758bc79469a
jun0222 <nextseason6@gmail.com>
2019-04-24 23:35:45 +0900

    add typing get miss or score

---
+++ typing/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+  const word = 'apple';
+  let loc = 0;
+  const target = document.getElementById('target');
+  target.textContent = word;
+
+  window.addEventListener('keyup', e => {
+    console.log(e.key);
+    if (e.key === word[loc]) {
+      console.log('score');
+    } else {
+      console.log('miss');
+    }
+  });
+}
